d504ffbe7cd63ba098aebd227d81ebe7dc3b9028
Jonathan Perry <jpper@umich.edu>
2021-07-13 20:58:10 +0900

    Finished implementing navbar & home page

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,25 @@
-import '../styles/globals.css'
 import { AppProps } from 'next/app'
 import axios from 'axios'
+import { Fragment } from 'react'
+import { useRouter } from 'next/router'
+// Relative
+import '../styles/tailwind.css'
+import Navbar from '../components/Navbar'
 
 axios.defaults.baseURL = 'http://localhost:5000/api'
 axios.defaults.withCredentials = true
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    const { pathname } = useRouter()
+    const authRoutes = ['/register', '/login']
+    const authRoute = authRoutes.includes(pathname)
+
+    return (
+        <Fragment>
+            {!authRoute && <Navbar />}
+            <Component {...pageProps} />
+        </Fragment>
+    )
 }
 
 export default App
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -31,7 +31,7 @@ export default function Register() {
     }
 
     return (
-        <div className="flex">
+        <div className="flex bg-white">
             <Head>
                 <title>Login</title>
             </Head>
