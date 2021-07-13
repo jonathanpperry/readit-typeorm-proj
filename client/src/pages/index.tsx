03435e2314ca079341a7aa39b64f1c220e56752e
Jonathan Perry <jpper@umich.edu>
2021-07-13 21:56:21 +0900

    Finished 9th video through 12 mins, successfully rendering out posts from db onto page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,47 @@
+import Axios from 'axios'
 import Head from 'next/head'
+import Link from 'next/link'
+import { Fragment, useEffect, useState } from 'react'
 
+import { Post } from '../types'
+
+// dayjs.extend(relativeTime)
 
 export default function Home() {
+    const [posts, setPosts] = useState<Post[]>([])
+
+    useEffect(() => {
+        Axios.get('/posts')
+            .then((res) => setPosts(res.data))
+            .catch((err) => console.log(err))
+    }, [])
+
     return (
-        <div>
+        <div className="pt-12">
             <Head>
                 <title>readit: the front page of the internet</title>
             </Head>
+            <div className="container flex pt-4">
+                {/* Posts feed */}
+                <div className="w-160">
+                    {posts.map((post) => (
+                        <div
+                            key={post.identifier}
+                            className="flex mb-4 bg-white rounded"
+                        >
+                            {/* Vote section */}
+                            <div className="w-10 text-center bg-gray-200 rounded-l">
+                                <p>V</p>
+                            </div>
+                            {/* Post data section */}
+                            <div className="w-full p-2">
+                                <p>{post.body}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                {/* Sidebar */}
+            </div>
         </div>
     )
 }
