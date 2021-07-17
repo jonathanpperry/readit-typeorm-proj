f808c2c4879e8d354d297449d125a8c3136cc3cb
Jonathan Perry <jpper@umich.edu>
2021-07-17 22:15:57 +0900

    Implemented uploading images

--- src/entities/Sub.ts
+++ src/entities/Sub.ts
@@ -1,10 +1,10 @@
 import {
-  Entity as TOEntity,
-  Column,
-  Index,
-  ManyToOne,
-  JoinColumn,
-  OneToMany,
+    Entity as TOEntity,
+    Column,
+    Index,
+    ManyToOne,
+    JoinColumn,
+    OneToMany,
 } from 'typeorm'
 
 import Entity from './Entity'
@@ -13,31 +13,34 @@ import Post from './Post'
 
 @TOEntity('subs')
 export default class Sub extends Entity {
-  constructor(sub: Partial<Sub>) {
-    super()
-    Object.assign(this, sub)
-  }
+    constructor(sub: Partial<Sub>) {
+        super()
+        Object.assign(this, sub)
+    }
 
-  @Index()
-  @Column({ unique: true })
-  name: string
+    @Index()
+    @Column({ unique: true })
+    name: string
 
-  @Column()
-  title: string
+    @Column()
+    title: string
 
-  @Column({ type: 'text', nullable: true })
-  description: string
+    @Column({ type: 'text', nullable: true })
+    description: string
 
-  @Column({ nullable: true })
-  imageUrn: string
+    @Column({ nullable: true })
+    imageUrn: string
 
-  @Column({ nullable: true })
-  bannerUrn: string
+    @Column({ nullable: true })
+    bannerUrn: string
 
-  @ManyToOne(() => User)
-  @JoinColumn({ name: 'username', referencedColumnName: 'username' })
-  user: User
+    @Column()
+    username: string
 
-  @OneToMany(() => Post, (post) => post.sub)
-  posts: Post[]
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'username', referencedColumnName: 'username' })
+    user: User
+
+    @OneToMany(() => Post, (post) => post.sub)
+    posts: Post[]
 }
